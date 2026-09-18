6be49b759f77f296a5afff60d739af87face672d
Jayant-Chaudhary <jayant10chaudhary@gmial.com>
2026-09-19 01:30:12 +0530

    config(server): initialize Supabase admin service-role client

---
+++ server/src/config/supabaseAdminClient.js
@@ -0,0 +1,11 @@
+const { createClient } = require('@supabase/supabase-js');
+const env = require('./env');
+
+const supabaseAdmin = createClient(env.supabaseUrl, env.supabaseServiceRoleKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+  },
+});
+
+module.exports = supabaseAdmin;
